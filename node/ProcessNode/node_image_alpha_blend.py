4885b76502a0e8ba3f6359ce091774e4d5766651
hackolite <laureote-loic@hotmail.fr>
2025-04-14 10:22:46 +0200

    add alpha_blend factory fix

--- node/ProcessNode/node_image_alpha_blend.py
+++ node/ProcessNode/node_image_alpha_blend.py
@@ -9,10 +9,8 @@ import dearpygui.dearpygui as dpg
 from node_editor.util import dpg_get_value, dpg_set_value
 
 from node.node_abc import DpgNodeABC
-
-#from node_editor.util import convert_cv_to_dpg
 from node.basenode import Node
-#from node.draw_node.draw_util.draw_util import draw_info
+
 
 def image_process(image1, image2, alpha_val, beta_val, gamma_val):
     image1_height, image1_width = image1.shape[:2]
@@ -70,40 +68,156 @@ class FactoryNode:
     def __init__(self):
         pass
 
-    
-    def add_node(self, parent, node_id, pos=[0, 0], callback=None, opencv_setting_dict=None):
-        """Ajoute un nœud au graphe de traitement."""
-        
-        # Génération des tags pour le Node et ses attributs
+    def add_node(
+        self,
+        parent,
+        node_id,
+        pos=[0, 0],
+        opencv_setting_dict=None,
+        callback=None,
+    ):
+
+
+
         node = Node()
+        node.tag_node_name = str(node_id) + ':' + node.node_tag
+        node.tag_node_input01_name = node.tag_node_name + ':' + node.TYPE_IMAGE + ':Input01'
+        node.tag_node_input01_value_name = node.tag_node_name + ':' + node.TYPE_IMAGE + ':Input01Value'
+        node.tag_node_input02_name = node.tag_node_name + ':' + node.TYPE_IMAGE + ':Input02'
+        node.tag_node_input02_value_name = node.tag_node_name + ':' + node.TYPE_IMAGE + ':Input02Value'
+        node.tag_node_input03_name = node.tag_node_name + ':' + node.TYPE_FLOAT + ':Input03'
+        node.tag_node_input03_value_name = node.tag_node_name + ':' + node.TYPE_FLOAT + ':Input03Value'
+        node.tag_node_input04_name = node.tag_node_name + ':' + node.TYPE_FLOAT + ':Input04'
+        node.tag_node_input04_value_name = node.tag_node_name + ':' + node.TYPE_FLOAT + ':Input04Value'
+        node.tag_node_input05_name = node.tag_node_name + ':' + node.TYPE_INT + ':Input05'
+        node.tag_node_input05_value_name = node.tag_node_name + ':' + node.TYPE_INT + ':Input05Value'
+        node.tag_node_output01_name = node.tag_node_name + ':' + node.TYPE_IMAGE + ':Output01'
+        node.tag_node_output01_value_name = node.tag_node_name + ':' + node.TYPE_IMAGE + ':Output01Value'
+        node.tag_node_output02_name = node.tag_node_name + ':' + node.TYPE_TIME_MS + ':Output02'
+        node.tag_node_output02_value_name = node.tag_node_name + ':' + node.TYPE_TIME_MS + ':Output02Value'
 
-        node.tag_node_name = f"{node_id}:{node.node_tag}"
-        tag_node_output01_name = f"{tag_node_name}:{node.TYPE_IMAGE}:Output01"
-        tag_node_output01_value_name = f"{tag_node_name}:{node.TYPE_IMAGE}:Output01Value"
 
-        # Initialisation du flux vidéo
-        node.cap = get_light_live_stream_url(VIDEO_ID)
-        node.last_frame_time = None
-        node.frame_time = 1.0 / 32  # 15 FPS pour une lecture fluide
-        node.small_window_w, node.small_window_h = 600, 400  # Taille de l'affichage
+        node._opencv_setting_dict = opencv_setting_dict
+        small_window_w = node._opencv_setting_dict['process_width']
+        small_window_h = node._opencv_setting_dict['process_height']
+        use_pref_counter = node._opencv_setting_dict['use_pref_counter']
 
-        # Image noire pour le démarrage
-        black_image = np.zeros((nodesmall_window_w, node.small_window_h, 3))
-        black_texture = node.convert_cv_to_dpg(black_image, node.small_window_w, node.small_window_h)
+        black_image = np.zeros((small_window_w, small_window_h, 3))
+        black_texture = node.convert_cv_to_dpg(
+            black_image,
+            small_window_w,
+            small_window_h,
+        )
 
-        # Création de la texture pour afficher l'image
         with dpg.texture_registry(show=False):
             dpg.add_raw_texture(
-                node.small_window_w, node.small_window_h, black_texture,
-                tag=tag_node_output01_value_name, format=dpg.mvFormat_Float_rgb
+                small_window_w,
+                small_window_h,
+                black_texture,
+                tag=node.tag_node_output01_value_name,
+                format=dpg.mvFormat_Float_rgb,
             )
 
-        # Création du nœud dans l'interface graphique
-        with dpg.node(tag=tag_node_name, parent=parent, label=node.node_label, pos=pos):
-            with dpg.node_attribute(tag=tag_node_output01_name, attribute_type=dpg.mvNode_Attr_Output):
-                dpg.add_image(tag_node_output01_value_name)
+
+        if node.tag_node_name not in node._slot_id:
+            node._slot_id[node.tag_node_name] = 1
+
+
+        with dpg.node(
+                tag=node.tag_node_name,
+                parent=parent,
+                label=node.node_label,
+                pos=pos,
+        ):
+
+            with dpg.node_attribute(
+            
+
+                    tag=node.tag_node_input01_name,
+                    attribute_type=dpg.mvNode_Attr_Input,
+            ):
+                dpg.add_text(
+                    tag=node.tag_node_input01_value_name,
+                    default_value='Input BGR image',
+                )
+
+            
+
+            with dpg.node_attribute(
+                    tag=node.tag_node_input02_name,
+                    attribute_type=dpg.mvNode_Attr_Input,
+            ):
+                dpg.add_text(
+                    tag=node.tag_node_input02_value_name,
+                    default_value='Input BGR image',
+                )
+
+            
+
+            with dpg.node_attribute(
+                    tag=node.tag_node_output01_name,
+                    attribute_type=dpg.mvNode_Attr_Output,
+            ):
+                dpg.add_image(node.tag_node_output01_value_name)
+
+            
+
+            with dpg.node_attribute(
+                    tag=node.tag_node_input03_name,
+                    attribute_type=dpg.mvNode_Attr_Input,
+            ):
+                dpg.add_slider_float(
+                    tag=node.tag_node_input03_value_name,
+                    label="alpha val",
+                    width=small_window_w - 80,
+                    default_value=node._alpha_default,
+                    min_value=node._alpha_min,
+                    max_value=node._alpha_max,
+                    callback=None,
+                )
+            
+
+            with dpg.node_attribute(
+                    tag=node.tag_node_input04_name,
+                    attribute_type=dpg.mvNode_Attr_Input,
+            ):
+                dpg.add_slider_float(
+                    tag=node.tag_node_input04_value_name,
+                    label="beta val",
+                    width=small_window_w - 80,
+                    default_value=node._beta_default,
+                    min_value=node._beta_min,
+                    max_value=node._beta_max,
+                    callback=None,
+                )
+            
+
+            with dpg.node_attribute(
+                    tag=node.tag_node_input05_name,
+                    attribute_type=dpg.mvNode_Attr_Input,
+            ):
+                dpg.add_slider_int(
+                    tag=node.tag_node_input05_value_name,
+                    label="gamma val",
+                    width=small_window_w - 80,
+                    default_value=node._gamma_default,
+                    min_value=node._gamma_min,
+                    max_value=node._gamma_max,
+                    callback=None,
+                )
+
+            if use_pref_counter:
+                with dpg.node_attribute(
+                        tag=node.tag_node_output02_name,
+                        attribute_type=dpg.mvNode_Attr_Output,
+                ):
+                    dpg.add_text(
+                        tag=node.tag_node_output02_value_name,
+                        default_value='elapsed time(ms)',
+                    )
 
         return node
+
 
 class Node(Node):
     _ver = '0.0.1'
@@ -127,140 +241,7 @@ class Node(Node):
     def __init__(self):
         pass
 
-    def add_node(
-        self,
-        parent,
-        node_id,
-        pos=[0, 0],
-        opencv_setting_dict=None,
-        callback=None,
-    ):
 
-        tag_node_name = str(node_id) + ':' + self.node_tag
-        tag_node_input01_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Input01'
-        tag_node_input01_value_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Input01Value'
-        tag_node_input02_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Input02'
-        tag_node_input02_value_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Input02Value'
-        tag_node_input03_name = tag_node_name + ':' + self.TYPE_FLOAT + ':Input03'
-        tag_node_input03_value_name = tag_node_name + ':' + self.TYPE_FLOAT + ':Input03Value'
-        tag_node_input04_name = tag_node_name + ':' + self.TYPE_FLOAT + ':Input04'
-        tag_node_input04_value_name = tag_node_name + ':' + self.TYPE_FLOAT + ':Input04Value'
-        tag_node_input05_name = tag_node_name + ':' + self.TYPE_INT + ':Input05'
-        tag_node_input05_value_name = tag_node_name + ':' + self.TYPE_INT + ':Input05Value'
-        tag_node_output01_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Output01'
-        tag_node_output01_value_name = tag_node_name + ':' + self.TYPE_IMAGE + ':Output01Value'
-        tag_node_output02_name = tag_node_name + ':' + self.TYPE_TIME_MS + ':Output02'
-        tag_node_output02_value_name = tag_node_name + ':' + self.TYPE_TIME_MS + ':Output02Value'
-
-
-        self._opencv_setting_dict = opencv_setting_dict
-        small_window_w = self._opencv_setting_dict['process_width']
-        small_window_h = self._opencv_setting_dict['process_height']
-        use_pref_counter = self._opencv_setting_dict['use_pref_counter']
-
-        black_image = np.zeros((small_window_w, small_window_h, 3))
-        black_texture = self.convert_cv_to_dpg(
-            black_image,
-            small_window_w,
-            small_window_h,
-        )
-
-        with dpg.texture_registry(show=False):
-            dpg.add_raw_texture(
-                small_window_w,
-                small_window_h,
-                black_texture,
-                tag=tag_node_output01_value_name,
-                format=dpg.mvFormat_Float_rgb,
-            )
-
-
-        if tag_node_name not in self._slot_id:
-            self._slot_id[tag_node_name] = 1
-
-
-        with dpg.node(
-                tag=tag_node_name,
-                parent=parent,
-                label=self.node_label,
-                pos=pos,
-        ):
-
-            with dpg.node_attribute(
-                    tag=tag_node_input01_name,
-                    attribute_type=dpg.mvNode_Attr_Input,
-            ):
-                dpg.add_text(
-                    tag=tag_node_input01_value_name,
-                    default_value='Input BGR image',
-                )
-
-            with dpg.node_attribute(
-                    tag=tag_node_input02_name,
-                    attribute_type=dpg.mvNode_Attr_Input,
-            ):
-                dpg.add_text(
-                    tag=tag_node_input02_value_name,
-                    default_value='Input BGR image',
-                )
-
-            with dpg.node_attribute(
-                    tag=tag_node_output01_name,
-                    attribute_type=dpg.mvNode_Attr_Output,
-            ):
-                dpg.add_image(tag_node_output01_value_name)
-
-            with dpg.node_attribute(
-                    tag=tag_node_input03_name,
-                    attribute_type=dpg.mvNode_Attr_Input,
-            ):
-                dpg.add_slider_float(
-                    tag=tag_node_input03_value_name,
-                    label="alpha val",
-                    width=small_window_w - 80,
-                    default_value=self._alpha_default,
-                    min_value=self._alpha_min,
-                    max_value=self._alpha_max,
-                    callback=None,
-                )
-            with dpg.node_attribute(
-                    tag=tag_node_input04_name,
-                    attribute_type=dpg.mvNode_Attr_Input,
-            ):
-                dpg.add_slider_float(
-                    tag=tag_node_input04_value_name,
-                    label="beta val",
-                    width=small_window_w - 80,
-                    default_value=self._beta_default,
-                    min_value=self._beta_min,
-                    max_value=self._beta_max,
-                    callback=None,
-                )
-            with dpg.node_attribute(
-                    tag=tag_node_input05_name,
-                    attribute_type=dpg.mvNode_Attr_Input,
-            ):
-                dpg.add_slider_int(
-                    tag=tag_node_input05_value_name,
-                    label="gamma val",
-                    width=small_window_w - 80,
-                    default_value=self._gamma_default,
-                    min_value=self._gamma_min,
-                    max_value=self._gamma_max,
-                    callback=None,
-                )
-            # 処理時間
-            if use_pref_counter:
-                with dpg.node_attribute(
-                        tag=tag_node_output02_name,
-                        attribute_type=dpg.mvNode_Attr_Output,
-                ):
-                    dpg.add_text(
-                        tag=tag_node_output02_value_name,
-                        default_value='elapsed time(ms)',
-                    )
-
-        return tag_node_name
 
     def update(
         self,
@@ -297,11 +278,13 @@ class Node(Node):
             slot_number = int(slot_number) - 1
             connection_type = connection_info[0].split(':')[2]
             connection_tag = connection_info[1].split(':')[3]
+            
+
             if connection_type == self.TYPE_FLOAT:
-                # 接続タグ取得
+
                 source_tag = connection_info[0] + 'Value'
                 destination_tag = connection_info[1] + 'Value'
-                # 値更新
+
                 input_value = round(float(dpg_get_value(source_tag)),3)
                 if connection_tag == 'Input03':
                     input_value = max([self._alpha_min, input_value])
@@ -310,6 +293,8 @@ class Node(Node):
                     input_value = max([self._beta_min, input_value])
                     input_value = min([self._beta_max, input_value])
                 dpg_set_value(destination_tag, input_value)
+            
+
             if connection_type == self.TYPE_INT:
 
                 source_tag = connection_info[0] + 'Value'
@@ -320,6 +305,8 @@ class Node(Node):
                     input_value = max([self._gamma_min, input_value])
                     input_value = min([self._gamma_max, input_value])
                 dpg_set_value(destination_tag, input_value)
+            
+
             if connection_type == self.TYPE_IMAGE:
 
                 connection_info_src = connection_info[0]
@@ -362,7 +349,7 @@ class Node(Node):
 
 
         if frame is not None:
-            texture = convert_cv_to_dpg(
+            texture = self.convert_cv_to_dpg(
                 frame,
                 small_window_w,
                 small_window_h,
